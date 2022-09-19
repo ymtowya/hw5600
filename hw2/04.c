0ab261c234102d1a45de90b2412985de76ef2358
ymtowya <kaigendo@126.com>
2022-09-19 08:46:00 -0700

    4

--- hw2/04.c
+++ hw2/04.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main() {
 
     int i = 0;
     char *lsArgv[3];
     lsArgv[0] = strdup("ls");
-    lsArgv[1] = strdup("/");
+    lsArgv[1] = strdup("/Users/mtowya/Desktop");
     lsArgv[2] = NULL;
 
-    for (; i < 5; ++i) {
+    for (; i < 6; ++i) {
         int childPid = fork();
         if (childPid < 0) {
             exit(1);
@@ -20,20 +21,26 @@ int main() {
             switch (i)
             {
             case 0:
-                execl("/bin/ls", "ls", "/", NULL);
+                execl("/bin/ls", "ls", "/Users/mtowya/Desktop", NULL);
                 break;
             case 1:
-                execle("/bin/ls", "ls", "/", NULL);
+                execlp("/bin/ls", "/bin/ls", "/Users/mtowya/Desktop", NULL);
                 break;
             case 2:
-                execlp("/bin/ls", "/bin/ls", "/", NULL);
+                execle("/bin/ls", "ls", "/Users/mtowya/Desktop", NULL);
                 break;
             case 3:
                 execv("/bin/ls", lsArgv);
+                break;
+            case 4:
+				execvp("ls", lsArgv);
+				break;
+			case 5:
+				execvpe("ls", lsArgv);
+				break;
             default:
                 break;
             }
-            
             exit(1);
         } else {
             // parent

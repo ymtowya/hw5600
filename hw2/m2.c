1670e9b3449a3715203a134373f32e446a4c524b
ymtowya <kaigendo@126.com>
2022-09-19 12:00:11 -0700

    7

--- hw2/m2.c
+++ hw2/m2.c
@@ -6,26 +6,27 @@
 #include <sys/time.h>
 #include <fcntl.h>
 #include <sched.h>
+#include <stdbool.h>
 
 int main() {
     const int loops  = 10;
+    struct timeval start, end;
 
-    cpu_set_t my_set;
-    CPU_ZERO(&my_set);
-    CPU_SET(0, &my_set);
-    sched_setaffinity(0, sizeof(cpu_set_t), &my_set);
+    //cpu_set_t my_set;
+    //CPU_ZERO(&my_set);
+    //CPU_SET(0, &my_set);
+    //sched_setaffinity(0, sizeof(cpu_set_t), &my_set);
 
     int p1[2], p2[2];
-    long time1 = 0, time2 = 0;
     char msg1[128], msg2[128];
     if (pipe(p1) == -1 || pipe(p2) == -1) {
         exit(1);
     }
 
     int pid = fork();
-    if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &my_set) == -1) {
-        exit(-1);
-    }
+    //if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &my_set) == -1) {
+    //    exit(-1);
+    //}
 
     if (fork < 0) {
         exit(1);
@@ -33,16 +34,16 @@ int main() {
         // child
         gettimeofday(&start, NULL);
         for (int i = 0; i < loops; ++i) {
-            read(p2, NULL, 0);
-            write(p1, NULL, 0);  
+            read(p2[0], msg1, 0);
+            write(p1[1], msg2, 0);  
         }
         gettimeofday(&end, NULL);
     } else {
         // parent
         gettimeofday(&start, NULL);
         for (int i = 0; i < loops; ++i) {
-            write(p2, NULL, 0);
-            read(p1, NULL, 0);
+            write(p2[1], msg1, 0);
+            read(p1[0], msg2, 0);
         }
         gettimeofday(&end, NULL);
     }

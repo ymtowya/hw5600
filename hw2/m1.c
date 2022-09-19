4ad2d6a13e1a1f4e651d7652e08f1b695cb8044e
ymtowya <kaigendo@126.com>
2022-09-19 11:36:03 -0700

    7

--- hw2/m1.c
+++ hw2/m1.c
@@ -18,10 +18,19 @@ int main() {
 
     for (int i = 0; i < loops; ++i) {
         gettimeofday(&start, NULL);
-        read(myFile, msg, 1);
+        read(myFile, msg, 0);
         gettimeofday(&end, NULL);
         printf("Start: %d - End: %d\nDuration: %d\n", start.tv_usec, end.tv_usec, end.tv_usec - start.tv_usec);
     }
+
+    printf("Next for an iteration in total:\n");
+    gettimeofday(&start, NULL);
+    for (int i = 0; i < loops; ++i) {
+        read(myFile, msg, 0);
+    }
+    gettimeofday(&end, NULL);
+    int durationTotal = end.tv_usec - start.tv_usec;
+    printf("Start: %d - End: %d\nTotal Duration: %d\nAvg Duration: %f\n", start.tv_usec, end.tv_usec, durationTotal, ((0.0 + durationTotal) / loops));
     
     close(myFile);
 

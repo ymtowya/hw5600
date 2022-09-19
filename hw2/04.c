0bc2ce19720ab674a8e46f2a741415f7f17ea583
ymtowya <kaigendo@126.com>
2022-09-19 23:31:49 +0800

    4

--- hw2/04.c
+++ hw2/04.c
@@ -6,23 +6,37 @@
 int main() {
 
     int i = 0;
+    char *lsArgv[3];
+    lsArgv[0] = strdup("ls");
+    lsArgv[1] = strdup("/");
+    lsArgv[2] = NULL;
+
     for (; i < 5; ++i) {
         int childPid = fork();
         if (childPid < 0) {
             exit(1);
         } else if (childPid == 0) {
+            // child
             switch (i)
             {
-            case /* constant-expression */:
-                /* code */
+            case 0:
+                execl("/bin/ls", "ls", "/", NULL);
                 break;
-            
+            case 1:
+                execle("/bin/ls", "ls", "/", NULL);
+                break;
+            case 2:
+                execlp("/bin/ls", "/bin/ls", "/", NULL);
+                break;
+            case 3:
+                execv("/bin/ls", lsArgv);
             default:
                 break;
             }
+            
             exit(1);
         } else {
-            printf("parent_%d waiting\n", i);
+            // parent
             wait(NULL);
         } 
     }

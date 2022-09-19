d7af5922ae43162db924bb8665a9419fee6e1683
ymtowya <kaigendo@126.com>
2022-09-19 22:54:01 +0800

    3

--- hw2/03.c
+++ hw2/03.c
@@ -5,18 +5,19 @@
 
 int main() {
     int childPid = fork();
-
+    int re = 0;
     if (childPid < 0) {
         exit(1); // failed fork
     } else if (childPid == 0) {
         // parent process
-        int re = waitpid(childPid, NULL, 0);
-        printf("goodbye");
+        re = waitpid(childPid, NULL, 0);
+        printf("goodbye\n");
         exit(1);
     } else {
         // child
-        print("hello");
+        printf("hello\n");
         exit(1);
     }
+    printf("%d\n", re);
     return 0;
 }
